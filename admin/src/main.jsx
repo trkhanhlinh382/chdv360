92d56c2ccd59b0c0fdf987ebc1eba5332bf1fc82
trkhanhlinh382 <trkhanhlinh382@gmail.com>
2026-05-24 19:51:42 +0700

    a

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,10 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const basename = import.meta.env.VITE_ROUTER_BASENAME || '/';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/admin">
+    <BrowserRouter basename={basename === '/' ? '' : basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
 )
+
